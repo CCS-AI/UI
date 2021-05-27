db82c4228e770e1f72e27ef70dfec915dbe1030e
Ofekroz10 <ofek@lightricks.com>
2021-05-27 11:29:43 +0300

    Add exam graph.

--- src/components/pages/Examination/PointType.tsx
+++ src/components/pages/Examination/PointType.tsx
@@ -1,7 +1,15 @@
 import React, { ReactElement, useState } from 'react';
 import Popup from 'reactjs-popup';
 import { IconButton, Button, TextField, Avatar } from '@material-ui/core';
-import { ExamPointTypes, examPointTypes, pointToImage, earTypes } from '../../../models/entities/examPointTypes';
+import {
+    ExamPointTypes,
+    examPointTypes,
+    pointToImage,
+    earTypes,
+    noResTypes,
+    NO_RES_SUFFIX,
+    typeWithNoRes
+} from '../../../models/entities/examPointTypes';
 import './Exam.css';
 import Checkbox from '@material-ui/core/Checkbox';
 import { Data } from './CreateExamination';
@@ -14,22 +22,24 @@ export const SelectPointType = ({ addData }: SelectPointTypeProps) => {
     let popups: ReactElement[] = [];
 
     earTypes.forEach((ear) => {
-        examPointTypes.map((key) => {
-            const type = key as ExamPointTypes;
-            const earSide = ear as 'RIGHT' | 'LEFT';
-            popups.push(
-                <Popup
-                    trigger={
-                        <IconButton>
-                            <Avatar variant="square" src={pointToImage(type, earSide)}></Avatar>
-                        </IconButton>
-                    }
-                    position={'bottom center'}
-                >
-                    <NewPointForm data={{ x: -1, y: -1, type: type, ear: earSide, isNoResponse: false }} addData={addData} />
-                </Popup>
-            );
-        });
+        examPointTypes
+            .filter((examType) => !examType.includes(NO_RES_SUFFIX))
+            .map((key) => {
+                const type = key as ExamPointTypes;
+                const earSide = ear as 'RIGHT' | 'LEFT';
+                popups.push(
+                    <Popup
+                        trigger={
+                            <IconButton>
+                                <Avatar variant="square" src={pointToImage(type, earSide)}></Avatar>
+                            </IconButton>
+                        }
+                        position={'bottom center'}
+                    >
+                        <NewPointForm data={{ x: -1, y: -1, type: type, ear: earSide, isNoResponse: false }} addData={addData} />
+                    </Popup>
+                );
+            });
     });
 
     return <div>{popups}</div>;
@@ -50,8 +60,10 @@ const NewPointForm = ({ data, addData }: NewPointForm) => {
     const submitForm = () => {
         try {
             validateInputs();
-            addData(state); // add real data!
-            console.log(`Added ${JSON.stringify(state)} to the graph (before normalized)`);
+            let newData = { ...state };
+            if (state.isNoResponse) newData.type = typeWithNoRes(state.type);
+            addData(newData); // add real data!
+            console.log(`Added ${JSON.stringify(newData)} to the graph (before normalized)`);
         } catch (e) {
             alert(e);
         }
@@ -73,17 +85,17 @@ const NewPointForm = ({ data, addData }: NewPointForm) => {
             <form style={{ direction: 'ltr' }}>
                 <div className={'formInput'}>
                     <label>X: </label>
-                    <TextField placeholder={'250'} onChange={(event) => setState({ ...state, x: Number(event.target.value) })}>
+                    <TextField placeholder={'250-8000'} onChange={(event) => setState({ ...state, x: Number(event.target.value) })}>
                         ofek
                     </TextField>
                 </div>
                 <div className={'formInput'}>
                     <label>Y: </label>
-                    <TextField placeholder={'1'} onChange={(event) => setState({ ...state, y: Number(event.target.value) })}>
+                    <TextField placeholder={'0-120'} onChange={(event) => setState({ ...state, y: Number(event.target.value) })}>
                         rac
                     </TextField>
                 </div>
-                <div>
+                <div hidden={!noResTypes.includes(state.type)}>
                     <label>No response: </label>
                     <Checkbox checked={state.isNoResponse} onChange={handleChange} inputProps={{ 'aria-label': 'primary checkbox' }} />
                 </div>
--- src/models/entities/examPointTypes.ts
+++ src/models/entities/examPointTypes.ts
@@ -10,13 +10,44 @@ import BCL_IMG from './../../static/images/PointTypeImages/BCL.jpeg';
 import BCL_MASK_IMG from './../../static/images/PointTypeImages/BCL+MASK.jpeg';
 import ST_IMG from './../../static/images/PointTypeImages/ST.jpeg';
 import STL_IMG from './../../static/images/PointTypeImages/STL.jpeg';
+import AC_MASK_NO_RES from './../../static/images/PointTypeImages/AC+MASK_NO_RES.jpeg';
+import AC_NO_RES from './../../static/images/PointTypeImages/AC_NO_RES.jpeg';
+import ACL_NO_RES from './../../static/images/PointTypeImages/ACL_NO_RES.jpeg';
+import BCL_NO_RES from './../../static/images/PointTypeImages/BCL_NO_RES.jpeg';
+import ACL_MASK_NO_RES from './../../static/images/PointTypeImages/ACL_MASK_NO_RES.png';
+import BC_NO_RES from './../../static/images/PointTypeImages/BC_NO_RES.png';
+import BC_MASK_NO_RES from './../../static/images/PointTypeImages/BC_MASK_NO_RES.png';
+import BCL_MASK_NO_RES from './../../static/images/PointTypeImages/BCL_MASK_NO_RES.png';
 import { Data } from '../../components/pages/Examination/CreateExamination';
 
 // Point type with Sound Flield or Stenger cannot be with isNoResponse = true
 export const earTypes = ['RIGHT', 'LEFT'];
-export const examPointTypes = ['AC', 'AC+MASK', 'BC', 'BC+MASK', 'Sound Flield', 'Stenger'];
+export const examPointTypes = [
+    'AC',
+    'AC+MASK',
+    'BC',
+    'BC+MASK',
+    'Sound Flield',
+    'Stenger',
+    'AC_NO_RES',
+    'AC+MASK_NO_RES',
+    'BC_NO_RES',
+    'BC+MASK_NO_RES'
+];
 const stroked_types = ['BC+MASK', 'BC', 'BCL', 'BCL+MASK'];
-export type ExamPointTypes = 'AC' | 'AC+MASK' | 'BC' | 'BC+MASK' | 'Sound Flield' | 'Stenger';
+export const NO_RES_SUFFIX = '_NO_RES';
+export type ExamPointTypes =
+    | 'AC'
+    | 'AC+MASK'
+    | 'BC'
+    | 'BC+MASK'
+    | 'Sound Flield'
+    | 'Stenger'
+    | 'AC_NO_RES'
+    | 'AC+MASK_NO_RES'
+    | 'BC_NO_RES'
+    | 'BC+MASK_NO_RES';
+export const noResTypes = ['AC', 'AC+MASK', 'BC', 'BC+MASK'];
 export const pointTypeToStyle = (ear: 'RIGHT' | 'LEFT', type: ExamPointTypes) => {
     const color = ear == 'RIGHT' ? 'red' : 'blue';
     const isStrokeDasharray = stroked_types.includes(type);
@@ -46,6 +77,14 @@ export const pointToImage = (type: ExamPointTypes, ear: 'RIGHT' | 'LEFT', isNoRe
         if (type == 'Sound Flield' && ear == 'LEFT') return SFL_IMG;
         if (type == 'Stenger' && ear == 'LEFT') return STL_IMG;
     } else {
+        if (type == 'AC_NO_RES' && ear == 'RIGHT') return AC_NO_RES;
+        if (type == 'AC_NO_RES' && ear == 'LEFT') return ACL_NO_RES;
+        if (type == 'AC+MASK_NO_RES' && ear == 'RIGHT') return AC_MASK_NO_RES;
+        if (type == 'AC+MASK_NO_RES' && ear == 'LEFT') return ACL_MASK_NO_RES;
+        if (type == 'BC_NO_RES' && ear == 'RIGHT') return BC_NO_RES;
+        if (type == 'BC_NO_RES' && ear == 'LEFT') return BCL_NO_RES;
+        if (type == 'BC+MASK_NO_RES' && ear == 'RIGHT') return BC_MASK_NO_RES;
+        if (type == 'BC+MASK_NO_RES' && ear == 'LEFT') return BCL_MASK_NO_RES;
         return AC_IMG;
     }
 };
@@ -61,4 +100,15 @@ export const getBaseType = (type: ExamPointTypes, ear: 'RIGHT' | 'LEFT') => {
     if (type == 'BC' && ear == 'RIGHT') return 'BC+MASK';
     if (type == 'BC' && ear == 'LEFT') return 'BC+MASK';
     return 'None';
+};
+
+export const typeWithNoRes = (type: ExamPointTypes) => {
+    if (!noResTypes.includes(type)) throw new Error('Invalid type.');
+
+    if (type == 'AC') return 'AC_NO_RES';
+    else if (type == 'AC+MASK') return 'AC+MASK_NO_RES';
+    else if (type == 'BC') return 'BC_NO_RES';
+    else if (type == 'BC+MASK') return 'BC+MASK_NO_RES';
+
+    throw new Error('Invalid type.');
 };
